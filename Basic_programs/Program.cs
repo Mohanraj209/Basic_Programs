b06359861383d70d2fb38e2fee108342917315bd
Mohanraj209 <mohanraj1715@gmail.com>
2022-02-23 00:00:54 +0530

    Displaying Message

---
+++ Basic_programs/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_programs
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("---------**** Basic_Core Programs ****---------");
+        }
+    }
+}
